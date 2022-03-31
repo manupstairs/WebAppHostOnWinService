ce383fe146feafa2c51ac48b0414520b1bd12f03
zheng_xing <Zheng.Xing@Dell.com>
2022-03-31 15:13:31 +0800

    make it can works as WindowsService

--- WebAppHostOnWinService/Program.cs
+++ WebAppHostOnWinService/Program.cs
@@ -1,7 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.Hosting.WindowsServices;
+
+var options = new WebApplicationOptions
+{
+    Args = args,
+    ContentRootPath = WindowsServiceHelpers.IsWindowsService() ? AppContext.BaseDirectory : default
+};
+
+var builder = WebApplication.CreateBuilder(options);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Host.UseWindowsService();
 
 var app = builder.Build();
 
